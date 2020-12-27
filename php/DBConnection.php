64a1a89e3f2b1ffcf37052282492c9ff27978681
ftrole <50705762+ftrole@users.noreply.github.com>
2020-12-27 21:00:04 +0100

    aggiunta a dbconnection e qualche fix

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -24,6 +24,28 @@ class DBAccess
 		}
 	}
 
+	/*
+	 * @param $query
+     * @return
+	*/
+    /**
+     * @param  $query: the query text
+     * @return null in case of query error, otherwise returns an array with the output of the query (can be an empty array if the query produces no output)
+     */
+    public function queryDB($query){
+        $result=null;
+        if($queryResult = $this->connessione->query($query)){
+            $result=array();
+            if($queryResult && $queryResult->num_rows>0){
+                while($row=$queryResult->fetch_array(MYSQLI_ASSOC)){
+                    array_push($result,$row);
+                }
+            }
+            $queryResult->close();
+        }
+        return $result;
+    }
+
 	public function closeDBConnection() 
 	{
 		if ($this->connection)
--- php/login.php
+++ php/login.php
@@ -1,69 +1,60 @@
 <?php
-require('sessione.php');
-
+require_once('sessione.php');
 
 /*Aggiunta header e menu*/
-$page= file_get_contents("../html/login.html");
+$page = file_get_contents("../html/login.html");
 
-$error='';
+$error = '';
 /* se ci sono valori in _POST cerca di fare il login o stampa errore */
-if(isset($_POST['email'])){
-    $email=$_POST['email'];
-    if(isset($_POST['password'])){
-        $pwd=$_POST['password'];
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    if (isset($_POST['password'])) {
+        $pwd = $_POST['password'];
     }
-    if(isset($_POST['remember_me'])){
-        $check='checked="checked"';
+    if (isset($_POST['remember_me'])) {
+        $check = 'checked="checked"';
     }
 
     /* crea connessione al DB */
-    require_once('connessione.php');
-    $obj_connection = new DBConnection();
+    require_once('DBConnection.php');
+    $obj_connection = new DBAccess();
 
-    if($obj_connection->create_connection()){
+    if ($obj_connection->openDBConnection()) {
 
-        $email=$obj_connection->escape_str(trim($email));
-        $hashed_pwd=hash("sha256",$obj_connection->escape_str(trim($pwd)));
+        $email = $obj_connection->trim($email);
+        $hashed_pwd = hash("sha256", $obj_connection->trim($pwd));
 
-        if(!$log_query=$obj_connection->connessione->query("SELECT * FROM utente WHERE Mail=\"$email\" AND PWD=\"$hashed_pwd\"")){
-            $error="[La query non è andata a buon fine]";
-        }else{
-            if(!$log_array=$obj_connection->queryToArray($log_query)){
-                $error="[Le credenziali inserite non sono corrette]";
-            }else{
-                $_SESSION['logged']=true;
-                $_SESSION['email']=$email;
-                $_SESSION['ID']=$log_array[0]['ID'];
-                $_SESSION['permesso']=$log_array[0]['Permessi'];
-
-                if(isset($_POST['remember_me'])){
-                    setcookie("user_email",$email,time()+60*60*24*30);
-                    setcookie("user_pwd",$pwd,time()+60*60*24*30);
-                }else{
-                    setcookie("user_email",$email,time()-3600);
-                    setcookie("user_pwd",$pwd,time()-3600);
-                }
-
-                $obj_connection->close_connection();
-
-                header('location: index.php');
-                exit;
+        //check to the db
+        $queryResult = $obj_connection->queryDB("SELECT * FROM utente WHERE Mail=\"$email\" AND PWD=\"$hashed_pwd\"")
+        if (!isset($queryResult)) {
+            $error = "[La query non è andata a buon fine]";
+        } else {
+            if (empty($queryResult)) {
+                $error = "[Le credenziali inserite non sono corrette]";
+            } else {
+                $_SESSION['logged'] = true;
+                $_SESSION['email'] = $email;
+                $_SESSION['ID'] = $queryResult[0]['ID'];
+                $_SESSION['permesso'] = $queryResult[0]['Permessi'];
             }
-        }
-        $obj_connection->close_connection();
 
-    }else{
-        $error=(new errore('DBConnection'))->printHTMLerror();
-    }
+            $obj_connection->close_connection();
+
+            header('location: index.php');
+            exit;
+        }
+} else {
+    $error = (new errore('DBConnection'))->printHTMLerror();
+}
 
 }
 
-$error=str_replace("[",'<p class="msg_box error_box">',$error);
-$error=str_replace("]","</p>",$error);
-$page=str_replace("%ERROR%",$error,$page);
-$page=str_replace("%VALUE_EMAIL%",$email,$page);
-$page=str_replace("%VALUE_PASSWORD%",$pwd,$page);
-$page=str_replace("%CHECKED%",$check,$page);
+$error = str_replace("[", '<p class="msg_box error_box">', $error);
+$error = str_replace("]", "</p>", $error);
+$page = str_replace("%ERROR%", $error, $page);
+$page = str_replace("%VALUE_EMAIL%", $email, $page);
+$page = str_replace("%VALUE_PASSWORD%", $pwd, $page);
+$page = str_replace("%CHECKED%", $check, $page);
 
 echo $page;
 ?>
