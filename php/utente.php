d8db06a98c35ff6de913e4eeaf2171997575428d
ftrole <50705762+ftrole@users.noreply.github.com>
2021-01-24 17:06:18 +0100

    sistemayo utente

--- php/utente.php
+++ php/utente.php
@@ -30,7 +30,7 @@ if ($obj_connection->openDBConnection()) {
         $altPic = $proPic[0]["alt_text"];
         $userInfo = "<div class=\"imgWrapper\"><img class=\"userPic\" src=\"$pathPic\" alt=\"$altPic\" /></div>
         <h2 class=\"userName\"> $username </h2>
-        <p class=\"email\"> La tua <span xml:lang=\"en\">E-mail</span>: $email </p>";
+        <p class=\"email\"> La tua <span xml:lang=\"en\">e-mail</span>: $email </p>";
     }
     $obj_connection->closeDBConnection();
 } else {
@@ -71,7 +71,7 @@ if ($_SESSION['permesso'] == 0) {
     $delete = "<form id=\"deleteAccount\" class=\"print-hide cambio-info\"
           method=\"post\"
           action=\"../php/utente.php\">
-        <fieldset class=\"form-fieldset fieldset-elimina-account\">
+        <fieldset class=\"formF\">
             <legend class=\"legend\">Eliminazione <span xml:lang=\"en\">account</span></legend>
             <ERRORI/>
             <label for=\"userPwd\">Per confermare inserisci la tua <span xml:lang=\"en\">password</span>:</label>
